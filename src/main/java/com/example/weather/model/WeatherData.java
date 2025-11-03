b14bd5413fd1325e9feaafe46828853df855c001
shakilmunavary <shakil.ahamed@gmail.com>
2025-11-03 22:15:29 +0530

    Create WeatherData.java

---
+++ src/main/java/com/example/weather/model/WeatherData.java
@@ -0,0 +1,7 @@
+public class WeatherData {
+    private String city;
+    private String temperature;
+    private String condition;
+    // Add other fields as per IndiaAPI response
+    // Getters and setters
+}
