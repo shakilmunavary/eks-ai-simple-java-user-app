7ec4837d4c259b7eb1a0e9b747400e6853a9fbb6
shakilmunavary <shakil.ahamed@gmail.com>
2025-11-03 23:54:22 +0530

    Create WeatherData.java

---
+++ src/main/java/com/model/WeatherData.java
@@ -0,0 +1,17 @@
+package com.model;
+
+public class WeatherData {
+    private String city;
+    private String temperature;
+    private String condition;
+
+    // Getters and setters
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public String getTemperature() { return temperature; }
+    public void setTemperature(String temperature) { this.temperature = temperature; }
+
+    public String getCondition() { return condition; }
+    public void setCondition(String condition) { this.condition = condition; }
+}
