4e1f715e6d1a7da98515837cbe4a299810d4313f
shakilmunavary <shakil.ahamed@gmail.com>
2025-11-03 22:34:47 +0530

    Update WeatherData.java

--- src/main/java/com/example/weather/model/WeatherData.java
+++ src/main/java/com/example/weather/model/WeatherData.java
@@ -1,7 +1,59 @@
+package com.example.weather.model;
+
 public class WeatherData {
     private String city;
     private String temperature;
     private String condition;
-    // Add other fields as per IndiaAPI response
+    private String humidity;
+    private String windSpeed;
+    private String observationTime;
+
     // Getters and setters
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(String windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getObservationTime() {
+        return observationTime;
+    }
+
+    public void setObservationTime(String observationTime) {
+        this.observationTime = observationTime;
+    }
 }
