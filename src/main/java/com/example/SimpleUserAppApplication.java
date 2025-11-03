1e2e5c7ad50fc3455bd63d3e847417381072e1c7
shakilmunavary <shakil.ahamed@gmail.com>
2025-11-03 23:26:55 +0530

    Update SimpleUserAppApplication.java

--- src/main/java/com/example/SimpleUserAppApplication.java
+++ src/main/java/com/example/SimpleUserAppApplication.java
@@ -1,8 +1,5 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SimpleUserAppApplication {
     public static void main(String[] args) {
